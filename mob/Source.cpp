653005818d3fe4c1fc2da28fbdfdd065a7eb195d
Andy Vu <phucvu95@gmail.com>
2018-10-07 19:25:47 -0700

    Removed Some redundancy
    Trimmed some parts of the code to remove some redundant calls within the code.

--- mob/Source.cpp
+++ mob/Source.cpp
@@ -1,6 +1,26 @@
 #include "f0700.h"
+#include <iostream>
 
 int main()
 {
+	int a1 = 64;
+	int a2[64];
+	for (int i = 0; i < 64; i++)
+	{
+		a2[i] = i;
+	}
+	a2[20] = 10;
+	int a3 = 2;
+	int a4[116];
+	for (int a = 0; a < 116; a++)
+	{
+		a4[a] = 0;
+	}
 
+	int test = f0700(a1, a2, a3, a4);
+
+	printf("%d", test);
+
+	system("pause");
+	return 0;
 }
--- mob/f0700.cpp
+++ mob/f0700.cpp
@@ -16,10 +16,9 @@ signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
 	//    a3: value shouldn't changed
 	//    a4: value can changed
 
-
 	int *v4;
 	int v5;
-	int v6;
+	int v6;	
 	int v7;
 	int o1;
 	int v9;
@@ -31,7 +30,7 @@ signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
 	int v15;
 	int a2a;
 
-	v4 = a2;
+	v4 = a2; //array
 	v5 = a1;
 	v6 = 0;
 	v7 = a2[a1];
@@ -39,38 +38,38 @@ signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
 	o1 = 0;
 	v9 = -1;
 	a2[a1 - 1] = 10000;
-	v15 = v7;
+	v15 = v7; //a2[a1] before change
 	a2[a1] = -10000;
 	a2a = 0;
 
-	if (a1 > 0)
-	{
+	//if (a1 > 0) //redundant
+	//{
 		v10 = a4;
 		do
 		{
-			v11 = (int)&v4[v6++];
-			*v10 = v11;
-			v12 = (int)&v4[v6];
-			if (v9 * (*(int *)v11 - *(int *)v12) < a3)
+			v11 = (int)&v4[v6++]; //points to the first index of v4
+			*v10 = v11; //current index of v10 is v11
+			v12 = (int)&v4[v6]; //points to the 2nd index of v4
+			if (v9 * (*(int *)v11 - *(int *)v12) < a3) // (-1 * v4[v6] - v4[v6 + 1]) < 2
 			{
 				do
 				{
-					if (v9 * *(int *)v12 > v9 * *(int *)*v10)
+					if (v9 * *(int *)v12 > v9 * *(int *)*v10) // -1 * v4[v6 + 1] > -1 * v4[v6]
 						*v10 = v12;
-					v13 = *(int *)(v12 + 4);
-					v12 += 4;
+					v13 = *(int *)(v12 + 4); // v13 = v4[v6 + 2]
+					v12 += 4; // v12 = third index of v4
 					++v6;
-				} while (v9 * (*(int *)*v10 - v13) < a3);
-				v5 = a1;
+				} while (v9 * (*(int *)*v10 - v13) < a3); // -1 * (a2[0] - (a2[v + 1]) < 2 
+				//v5 = a1; //redundant
 			}
-			++v10;
-			v9 = -v9;
-			o1 = a2a++ + 1;
-		} while (v6 < v5);
-	}
+			++v10; //2nd index of v4
+			v9 = -v9; //v9 = 1
+			o1 = a2a++ + 1; // o1 = a2a + 1; a2a++;
+		} while (v6 < v5); // v6 increases 2 each time. v5 is always a1
+	//}
 	if (o1 % 2)
 		--o1;
-	v4[v5 - 1] = v14;
-	v4[v5] = v15;
+	v4[v5 - 1] = v14; //redundant?
+	v4[v5] = v15; //redundant?
 	return o1;
 }
