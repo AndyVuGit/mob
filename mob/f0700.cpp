e24a06d5b39b75e2296b2d9e1caea28f0dab9d8a
Andy Vu <phucvu95@gmail.com>
2018-10-07 21:51:19 -0700

    Reformat
    Reformatted to look better

--- mob/f0700.cpp
+++ mob/f0700.cpp
@@ -44,30 +44,30 @@ signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
 
 	//if (a1 > 0) //redundant
 	//{
-		v10 = a4;
-		do
+	v10 = a4;
+	do
+	{
+		v11 = &v4[v6]; //points to the first index of v4 //Changed 
+		++v6; //Added
+		v10 = v11; //current index of v10 is v11 //Changed
+		v12 = &v4[v6]; //points to the 2nd index of v4 //Changed
+		if (a3 > v9 * ((*v11) - (*v12))) // (-1 * v4[v6] - v4[v6 + 1]) < 2 //Changed
 		{
-			v11 = &v4[v6]; //points to the first index of v4 //Changed 
-			++v6; //Added
-			v10 = v11; //current index of v10 is v11 //Changed
-			v12 = &v4[v6]; //points to the 2nd index of v4 //Changed
-			if (a3 > v9 * ((*v11) - (*v12))) // (-1 * v4[v6] - v4[v6 + 1]) < 2 //Changed
+			do
 			{
-				do
-				{
-					if (v9 * (*v12) > v9 * (*v10)) // -1 * v4[v6 + 1] > -1 * v4[v6] //Changed
-						v10 = v12; //Changed
-					v13 = *(v12 + 1); // v13 = v4[v6 + 2] //Changed
-					v12 += 1; // v12 = third index of v4
-					++v6;
-				} while (a3 > v9 * (*v10 - v13)); // -1 * (a2[0] - (a2[v + 1]) < 2  //Changed
-				//v5 = a1; //redundant
-			}
-			++v10; //2nd index of v4
-			v9 = -v9; //v9 = 1
-			o1 = a2a + 1; // o1 = a2a + 1; a2a++;
-			++a2a;
-		} while (v6 < v5); // v6 increases 2 each time. v5 is always a1
+				if (v9 * (*v12) > v9 * (*v10)) // -1 * v4[v6 + 1] > -1 * v4[v6] //Changed
+					v10 = v12; //Changed
+				v13 = *(v12 + 1); // v13 = v4[v6 + 2] //Changed
+				v12 += 1; // v12 = third index of v4
+				++v6;
+			} while (a3 > v9 * (*v10 - v13)); // -1 * (a2[0] - (a2[v + 1]) < 2  //Changed
+			//v5 = a1; //redundant
+		}
+		++v10; //2nd index of v4
+		v9 = -v9; //v9 = 1
+		o1 = a2a + 1; // o1 = a2a + 1; a2a++;
+		++a2a;
+	} while (v6 < v5); // v6 increases 2 each time. v5 is always a1
 	//}
 	if (o1 % 2)
 		--o1;
