f314a9d471ae50b13400b0c8403c53af18facbb3
Andy Vu <phucvu95@gmail.com>
2018-10-09 13:25:55 -0700

    Updated Project from VS 2013 to VS 2017
    Made another version of the project in visual studio 2017.

---
+++ 2017 version/mobTest/mobTest/main.cpp
@@ -0,0 +1,29 @@
+#include "f0700.h"
+#include <iostream>
+
+int main()
+{
+	//Sample Input
+	int a1 = 128;
+	int a2[1408];
+	for (int i = 0; i < 1408; i++)
+	{
+		a2[i] = i;
+	}
+	a2[20] = -400;
+	a2[30] = 400;
+	a2[500] = 0;
+	int a3 = 4;
+	int a4[116];
+	for (int a = 0; a < 116; a++)
+	{
+		a4[a] = 0;
+	}
+
+	int test = f0700(a1, a2, a3, a4);
+	int prev = f0700Original(a1, a2, a3, a4);
+
+	std::cout << "test = " << test << std::endl;
+	std::cout << "prev = " << prev << std::endl;
+	return 0;
+}
